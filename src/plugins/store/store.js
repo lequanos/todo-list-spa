5d358efbb8c7b93cffb48d51d0f72e0d710c5468
Quan <lequanos@hotmail.com>
2023-01-24 21:18:45 +0100

    add i18n

--- src/plugins/store/store.js
+++ src/plugins/store/store.js
@@ -1,10 +1,16 @@
 import { legacy_createStore as createStore, compose } from 'redux';
+
 import reducer from './reducers';
+import i18n from '../lang/i18n';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancers = composeEnhancers();
 
 const store = createStore(reducer, enhancers);
+
+store.subscribe(() => {
+  i18n.changeLanguage(store.getState().user.lang);
+});
 
 export default store;
